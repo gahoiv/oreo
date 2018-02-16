976654321f9bc5bdddcf968ad19bde4210520156
gahoiv <gahoi.vishwas@gmail.com>
2018-02-16 07:59:28 +0530

    For adding more content

--- src/main/frontend/src/component/what-we-do.js
+++ src/main/frontend/src/component/what-we-do.js
@@ -12,7 +12,7 @@ const card = (
           <Col sm={10} md={12} >Tech Consultants</Col>
       </Row>
       <Row className='rowHeader-desc'>
-          <Col sm={10} md={12} >Building, maintaining, and upgrading Web-platforms has become so tough. We replace tough with easy.</Col>
+          <Col sm={10} md={12} >Building, maintaining, and upgrading Web-platforms has become so tough. We replace tough with easy. We can help you build an idea from scratch as well as upgrade an existing Website/Web platform.</Col>
       </Row> 
     </div>
   );
@@ -26,7 +26,7 @@ const card = (
           <Col sm={10} md={12} >Experience and Vision</Col>
       </Row>
       <Row className='rowHeader-desc'>
-          <Col sm={10} md={12} >A decade of experience in Web and Content makes us what? Your future best friends in tech.</Col>
+          <Col sm={10} md={12} >A decade of experience in Web and Content makes us what? Your future best friends in tech. We have build Startups and have spent considerable time in the Corporate honing our skills.</Col>
       </Row> 
     </div>
   );
@@ -40,7 +40,7 @@ const card = (
           <Col sm={10} md={12} >Consultancy and Support</Col>
       </Row>
       <Row className='rowHeader-desc'>
-          <Col sm={10} md={12} >We will help you plan, build and scale your existing Web platforms or help build a new one.</Col>
+          <Col sm={10} md={12} >We will help you plan, build and scale your existing Web platforms or help build a new one. Advising and Consulting on a regular basis, we help keep a check on your Tech progress and complexities.</Col>
       </Row> 
     </div>
   );
